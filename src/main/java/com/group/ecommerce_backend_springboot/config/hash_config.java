1c55cff67e9a810c914470cd92e2e42c84ee1642
Huy Duong <duongquanghuy2202@gmail.com>
2025-02-11 16:03:48 +0700

    hashing password

---
+++ src/main/java/com/group/ecommerce_backend_springboot/config/hash_config.java
@@ -0,0 +1,14 @@
+package com.group.ecommerce_backend_springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class hash_config {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
