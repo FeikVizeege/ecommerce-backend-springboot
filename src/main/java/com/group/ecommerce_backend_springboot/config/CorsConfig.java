73eb780f46dc0644ee7b90d8de367ab768386e7e
Huy Duong <duongquanghuy2202@gmail.com>
2025-03-04 14:58:10 +0700

    Config changes

--- src/main/java/com/group/ecommerce_backend_springboot/config/CorsConfig.java
+++ src/main/java/com/group/ecommerce_backend_springboot/config/CorsConfig.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class cors_config implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/api/**")
